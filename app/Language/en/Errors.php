76036913c75bc896d16337dbebb62a3e37d209b7
akcoders <akcoders@users.noreply.github.com>
2026-05-22 17:52:09 +0530

    Fix production error messages and controller boot issue

---
+++ app/Language/en/Errors.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'whoops'           => 'Whoops!',
+    'weHitASnag'       => 'We seem to have hit a snag. Please try again shortly.',
+    'pageNotFound'     => '404 - Page Not Found',
+    'sorryCannotFind'  => 'Sorry, we cannot find the page you were looking for.',
+    'badRequest'       => '400 - Bad Request',
+    'sorryBadRequest'  => 'Sorry, your request could not be understood or processed.',
+];
